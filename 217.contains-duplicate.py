ad6ecfca7f1cb841c1a3d7cb0fbae07c392dc704
Rahat Ali <ali.rahat@pm.me>
2023-06-05 20:52:53 +0100

    Leetcode Problem #217 (Easy): Contains Duplicate

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for n in range(len(nums)-1):
+            if nums[n] == nums[n+1]: return True
+        return False
+# @lc code=end
+
