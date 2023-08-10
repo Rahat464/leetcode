62b80f205d4cd743e921d8edf686bf899efbfcbb
Rahat Ali <ali.rahat@pm.me>
2023-08-10 18:27:24 +0100

    Added comments explaining the code

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -12,15 +12,22 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # In the reversed list
+        # the last pointer is null hence the node before the current first will be null
         prev = None
         curr = head
 
+        # If the next node to be reversed (curr) does not exist 
+        # it means that curr is the last node in the list
         while curr is not None:
+            # shift all pointers by one to the right (next)
             next = curr.next
             curr.next = prev
             prev = curr
             curr = next
 
+            # Check that the next node does exist
+            # to prevent AttributeError
             if next is not None: next = curr.next
 
         return prev
