9c6c7a93727a9071810d2bc0d8a1e64360c02d42
Rahat Ali <ali.rahat@pm.me>
2023-08-11 12:30:03 +0100

    Leetcode 169: majority element 47/47 cases passed (154 ms) Your runtime beats 86.51 % of python3 submissions Your memory usage beats 70.56 % of python3 submissions (17.9 MB)

---
+++ 169.majority-element.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # My initial solution was to use a hashmap to count the number of occurrences of each number
+        # Then return the key with largest value
+
+        '''
+        My initial solution was to use a hashmap to count the no. of occurrences of each number
+        Then return the key with largest value
+        '''
+
+        # Using Boyer-Moore Voting Algorithm
+
+        # Holds most common value
+        candidate = nums[0]
+        # Holds the number of occurrences of the current candidate
+        # It counts how many more occurrences the current candidate has compared to other candidates
+        count = 0
+
+        for n in nums:
+            # Increment occurrence if it matches
+            if candidate == n:
+                count += 1
+            # Decrement if it's another value
+            else:
+                count -= 1
+            
+            # If count is negative, it means that there is a more common number
+            if count == 0:
+                candidate = n
+                count += 1
+
+        return candidate
+        
+# @lc code=end
+
