13bdb478d26445e40a7abf7109fa521d86f74436
Rahat Ali <ali.rahat@pm.me>
2023-08-19 16:53:01 +0100

    114/114 cases passed (38 ms) Your runtime beats 82.9 % of python3 submissions Your memory usage beats 40.59 % of python3 submissions (16.3 MB) - Now with comments explaining the solution

--- 844.backspace-string-compare.py
+++ 844.backspace-string-compare.py
@@ -7,19 +7,29 @@
 # @lc code=start
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
+        # Values are cached since they will be used often in the loop
+        # and to prevent calling len() function multiple times which would increase runtime
         len_s, len_t = len(s), len(t)
+        # Stores strings with characters removed
         newS = newT = ""
         
+        # Find longer string and keep looping until both have been iterated over
+        # This is so that we can go over both strings in one iteration
+        # instead of having two different loops for each string
         for i in range(0, max( len(s), len(t))):
+            # Make sure index is still in range, if not assign empty string
             c_s = s[i] if i<len_s else ""
             c_t = t[i] if i<len_t else ""
 
+            # Append curr. char to string
+            # If backspace, remove previous character and do not append anything this iteration
             if c_s != "#": newS += c_s
             else: newS = newS[:-1]
 
             if c_t != "#": newT += c_t
             else: newT = newT[:-1]
 
+        # See if both processed strings match
         return True if newS == newT else False
 
                        
