8cddb7d90336e52acf4016464a583657c322088f
Rahat Ali <44652188+Rahat464@users.noreply.github.com>
2023-09-25 14:01:53 +0100

    54/54 cases passed (44 ms) Your runtime beats 41.02 % of python3 submissions Your memory usage beats 43.09 % of python3 submissions (16.3 MB)

---
+++ 389.find-the-difference.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=389 lang=python3
+#
+# [389] Find the Difference
+#
+
+# @lc code=start
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        freq = {}
+        for c in s:
+            if c not in freq: freq[c] = 1
+            else: freq[c] += 1
+        
+        for c in t:
+            if c in freq: freq[c] -= 1
+            else: return c
+        
+        for k, v in freq.items():
+            if v < 0: return k
+            
+        
+# @lc code=end
+
