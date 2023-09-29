e80191dbcf101d204f5fd5a77968301ac4c44742
Rahat Ali <ali.rahat@pm.me>
2023-09-29 10:40:01 +0100

    371/371 cases passed (774 ms) Your runtime beats 99.34 % of python3 submissions Your memory usage beats 6.11 % of python3 submissions (31.2 MB)

--- 896.monotonic-array.py
+++ 896.monotonic-array.py
@@ -7,18 +7,19 @@
 # @lc code=start
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        monotone = None
+        increasing = None
         i = 0
-        prev = nums[0]
 
-        while monotone is None:
+        while increasing is None:
             if i+1 < len(nums):
-                if nums[i] < nums[i+1]: monotone = "increasing"
-                elif nums[i] > nums[i+1]: monotone = "decreasing"
+                if nums[i] < nums[i+1]: increasing = True
+                elif nums[i] > nums[i+1]: increasing = False
                 else: i += 1
             else: return True
 
-        if monotone == "increasing":
+        prev = nums[0]
+
+        if increasing is True:
             for n in nums[i:]:
                 if n < prev: return False
                 else: prev = n
