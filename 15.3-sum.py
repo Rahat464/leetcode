639384dabac54e1f41999ab2600e50798b5a0df0
Rahat Ali <ali.rahat@pm.me>
2023-10-05 17:57:10 +0100

    312/312 cases passed (9243 ms) Your runtime beats 5 % of python3 submissions Your memory usage beats 79.09 % of python3 submissions (20.4 MB) - Cleaning up code

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -14,11 +14,7 @@ class Solution:
         nums.sort()
 
         for i, n in enumerate(nums):
-            # Skip all duplicates
-            # if n < 0 and nums[i + 1] == n: continue
-
-            # i becomes l pointer
-            i += 1
+            i += 1 # i becomes l pointer
             r = len(nums) - 1
 
             # Keep looping until combination haven't been found and pointer haven't met
@@ -27,7 +23,6 @@ class Solution:
                 if total == 0 and ([n, nums[i], nums[r]] not in res):
                     res.append([n, nums[i], nums[r]])
                 else:
-                    # TODO: While loop until total is no longer larger
                     if total > 0: r -= 1
                     else: i += 1
                 
