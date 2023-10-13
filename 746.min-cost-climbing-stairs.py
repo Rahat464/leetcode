31dace9016bf22959c30a66791efa05beed6e736
Rahat Ali <44652188+Rahat464@users.noreply.github.com>
2023-10-13 15:24:57 +0100

    Solution by Neetcode 283/283 cases passed (55 ms) Your runtime beats 86.77 % of python3 submissions Your memory usage beats 94.79 % of python3 submissions (16.3 MB)

---
+++ 746.min-cost-climbing-stairs.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=746 lang=python3
+#
+# [746] Min Cost Climbing Stairs
+#
+
+# @lc code=start
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+
+        return cost[0] if cost[0] < cost[1] else cost[1]
+        
+# @lc code=end
+
