9548896e9376aebd14ab9212dd480d4492a5a002
Rahat Ali <ali.rahat@pm.me>
2023-12-24 10:06:48 +0000

    89/89 cases passed (36 ms) Your runtime beats 98.74 % of python3 submissions Your memory usage beats 5.67 % of python3 submissions (17.5 MB) - Added comments

--- 1758.minimum-changes-to-make-alternating-binary-string.py
+++ 1758.minimum-changes-to-make-alternating-binary-string.py
@@ -11,9 +11,14 @@ class Solution:
         changes = 0
 
         for i in range(len(s)):
+            # If the index is even, the character should be 0
+            # If the index is odd, the character should be 1
+            # If the character is not what it should be, we need to change it
             if (i % 2 == 0 and s[i] == '1') or (i % 2 == 1 and s[i] == '0'):
                 changes += 1
         
+        # We already know the minimum number of changes if the string started with 0
+        # We can find the complement and compare the number of changes needed
         return min(changes, len(s) - changes)
 # @lc code=end
 
