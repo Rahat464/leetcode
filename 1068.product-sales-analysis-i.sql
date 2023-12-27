eb2c7a713a95a80404624fa19616abbadf24a870
Rahat Ali <44652188+Rahat464@users.noreply.github.com>
2023-12-27 19:21:41 +0000

    11/11 cases passed (2498 ms) Your runtime beats 18.26 % of mysql submissions Your memory usage beats 100 % of mysql submissions (0B)

---
+++ 1068.product-sales-analysis-i.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=1068 lang=mysql
+--
+-- [1068] Product Sales Analysis I
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT
+    Product.product_name,
+    Sales.year,
+    Sales.price
+FROM Sales
+INNER JOIN Product ON Sales.product_id = Product.product_id;
+-- @lc code=end
+
