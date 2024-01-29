b2a573e63dd8c9ae29a04e94464650d663921941
Rahat Ali <44652188+Rahat464@users.noreply.github.com>
2024-01-29 12:53:28 +0000

    22/22 cases passed (33 ms) Your runtime beats 79.62 % of python3 submissions Your memory usage beats 68.49 % of python3 submissions (16.6 MB) Improved O(1) amortised solution

--- 232.implement-queue-using-stacks.py
+++ 232.implement-queue-using-stacks.py
@@ -6,14 +6,50 @@
 
 # @lc code=start
 class MyQueue:
-
+    # We push to enqueue to stack1 and dequeue from stack2
+    # When stack2 is empty, we reverse stack1 into stack2
+    # This will lead to O(1) amortized time complexity for push and pop
     def __init__(self):
-        # stack1 will be used to store the list as LIFO
-        # stack2 will be used as a "container" to reverse the stack and turn it into FIFO
         self.stack1 = []
         self.stack2 = []
+
+    def push(self, x: int) -> None: # aka enqueue
+        self.stack1.append(x)
+    
+    # Helper function to reverse stack1 into stack2
+    def reverse(self):
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
         
 
+    def pop(self) -> int:
+        if not self.stack2: self.reverse()
+        
+        # Pop from stack2
+        # Alternate solution would be to use built in pop() method
+        tmp = self.stack2[-1]    
+        del self.stack2[-1]
+        return tmp
+
+    def peek(self) -> int:
+        if not self.stack2: self.reverse()
+
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        return True if not (self.stack1 or self.stack2) else False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+# @lc code=end
+
+""" Old O(n) soluion
     def push(self, x: int) -> None:
         # While stack1 is not empty, reverse the stack by using stack2 as a container
         while self.stack1:
@@ -26,7 +62,6 @@ class MyQueue:
         while self.stack2:
             self.stack1.append(self.stack2.pop())
 
-
     def pop(self) -> int:
         top_element = self.stack1[-1]
         del self.stack1[-1]
@@ -37,14 +72,4 @@ class MyQueue:
 
     def empty(self) -> bool:
         return not self.stack1
-        
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
-# @lc code=end
-
+"""
