e48e9d48a0cb143ade44514080dce15c1d47416a
Rahat Ali <44652188+Rahat464@users.noreply.github.com>
2024-02-14 17:22:17 +0000

    63/63 cases passed (138 ms) Your runtime beats 54.17 % of python3 submissions Your memory usage beats 95.93 % of python3 submissions (30.8 MB)

---
+++ 1448.count-good-nodes-in-binary-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=1448 lang=python3
+#
+# [1448] Count Good Nodes in Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        # DFS with variable to keep track of smallest number found so far
+
+        return self.dfs(root, root.val)
+    
+    def dfs(self, root, max_val):
+        if not root: return 0
+
+        count = 1 if root.val >= max_val else 0
+
+        max_val = max(max_val, root.val)
+
+        return count + self.dfs(root.left, max_val) + self.dfs(root.right, max_val)
+        
+# @lc code=end
+
