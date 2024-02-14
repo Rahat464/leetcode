92954b84a063344656118ef204e27c90ae27d102
Rahat Ali <44652188+Rahat464@users.noreply.github.com>
2024-02-14 17:24:04 +0000

    Fixed comment from "smallest" to "largest"

--- 1448.count-good-nodes-in-binary-tree.py
+++ 1448.count-good-nodes-in-binary-tree.py
@@ -13,17 +13,13 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        # DFS with variable to keep track of smallest number found so far
-
+        # DFS with variable to keep track of largest number found so far
         return self.dfs(root, root.val)
     
     def dfs(self, root, max_val):
         if not root: return 0
-
         count = 1 if root.val >= max_val else 0
-
         max_val = max(max_val, root.val)
-
         return count + self.dfs(root.left, max_val) + self.dfs(root.right, max_val)
         
 # @lc code=end
