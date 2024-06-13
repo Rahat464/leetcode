44772ba8465f23a7e97fc074cf2c867d8e3fcc0b
Rahat Ali <ali.rahat6464+git@gmail.com>
2024-06-13 10:26:30 +0100

    262/262 cases passed (57 ms) Your runtime beats 73.07 % of python3 submissions Your memory usage beats 82.54 % of python3 submissions (16.4 MB)

---
+++ 2037.minimum-number-of-moves-to-seat-everyone.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=2037 lang=python3
+#
+# [2037] Minimum Number of Moves to Seat Everyone
+#
+
+# @lc code=start
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats = sorted(seats)
+        students = sorted(students)
+        res = 0
+
+        for i in range(len(seats)):
+            res += abs(seats[i] - students[i])
+        
+        return res
+        
+# @lc code=end
+
