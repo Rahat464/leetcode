83df604cde3eafee3945468050e302d11228273e
Rahat Ali <rahat@rahatali.dev>
2024-12-27 16:55:44 +0000

    Add alternative solution for 217  with O(n) time complexity

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -7,6 +7,7 @@
 # @lc code=start
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Solution 1: O(nlogn) time and O(1) memory
         # Sort numbers in ascending order
         nums.sort()
         for n in range(len(nums)-1):
@@ -14,5 +15,15 @@ class Solution:
             # If equal, it means there is a duplicate
             if nums[n] == nums[n+1]: return True
         return False
+    
+        """
+        #Solution 2: O(n) time and memory
+        seen = set()
+            for num in nums:
+                if num in seen:
+                    return True
+                seen.add(num)
+            return False
+        """
 # @lc code=end
 
